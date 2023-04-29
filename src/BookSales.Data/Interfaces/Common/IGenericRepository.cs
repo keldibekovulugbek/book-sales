44af3981f57b3680136fa2a3c6d621277881165f
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2023-04-29 12:18:23 +0500

    added generic interface

---
+++ src/BookSales.Data/Interfaces/Common/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookSales.Application.Utils;
+using BookSales.DataAccess.Common;
+using BookSales.Domain.Common;
+
+namespace BookSales.DataAccess.Interfaces.Common;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    public Task<T> CreateAsync(T entity);
+    public Task<T> DeleteAsync(long id);
+    public Task<T?> FindByIdAsync(long id);
+    public Task<PagedList<T>> GetAllAsync(PaginationParams @params);
+    public Task<T> UpdateAsync(long id, T entity);
+}
